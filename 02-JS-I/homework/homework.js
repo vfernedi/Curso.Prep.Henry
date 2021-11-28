505d6173d4e1c8c46b45716c2a0fdcb64ff8982c
vfernedi <vfernedi@gail.com>
2021-11-28 15:37:38 -0500

    number commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = 'hello';
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = 65;
 
 // Crea una variable booleana:
 const nuevoBool = null;
